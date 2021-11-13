b11661f2c93cfd6ce71d5a1d63c653cfdb9a14c7
Demner <rlopezcr@everis.com>
2021-11-13 18:36:45 -0500

    refactorizacion de shell_script_windows.sh

--- shell_script._windows.sh
+++ shell_script._windows.sh
@@ -1,13 +1,12 @@
 #!/bin/ksh
-NAME_SHELL="SH_ProcesaTareaProgCBIO"
+NAME_SHELL="SH_TEST"
 #*********************************************************************
-#* DESCRIPCION      	: Invocacion de /home/weblogic/shells/$NAME_SHELL 
+#* DESCRIPCION      	: Invocacion de SH_TEST
 #* FECHA                : 17/02/2020 	                                        
 #* VERSION             	: 1.0	                                        
 #************************************************************************
 clear
-
-#Setup:
+#Setup VARSET:
 RUTA_VARSET="C:\\gits\\$NAME_SHELL\\remote\\BIN\\.varset_windows"
 
 . $RUTA_VARSET
@@ -17,22 +16,12 @@ FECHA_LOG=`date +"%d%m%Y"`
 TRANSACTION=`date +%Y%m%d%H%M%S`
 FILELOG=$DIR_LOG\\${LOG_NAME}.log
 LOGDATE=`date +"%d-%m-%Y %H:%M:%S"`
-USR=`whoami`	
+USR=`whoami`
 
-#VARIABLES ENVIADAS
-FECHA_PROG=`date +"%Y-%m-%d"`
-SERVC_ESTADO="1"
-SERVI_COD="75"
-TIPO_OPERACION="X"
 export properties=$PROPERTIES_PATH
-
-
-
 ############### INICIO - EJECUCION DE SHELL ###############
 echo "${LOGDATE} INFO  @ [INICIO de ejecucion de la Shell - $NAME_SHELL -][`date +%Y-%m-%d@%H:%M:%S`]"
-
 ############### 2 - EJECUCION DE JAR ###############
-#Launch:
 echo "${DATE} INFO  @ [INICIO del proceso][`date +%Y-%m-%d@%H:%M:%S`]"
 echo "${DATE} INFO  @ Usuario: $USR "  
 echo "${DATE} INFO  @ Transaccion: ${TRANSACTION}" 
@@ -41,17 +30,10 @@ echo "${DATE} INFO  @ [INICIO de la ejecucion de la Shell][`date +%Y-%m-%d@%H:%M
 echo "${DATE} INFO  @ PARAMETROS A ENVIAR A:" 
 echo "*****************************************************"
 echo "${DATE} INFO  @ Transaccion: ${TRANSACTION}" 
-echo "${DATE} INFO  @ Fecha prog : ${FECHA_PROG}" 
-echo "${DATE} INFO  @ ServcEstado : ${SERVC_ESTADO}" 
-echo "${DATE} INFO  @ ServiCod : ${SERVI_COD}" 
-echo "${DATE} INFO  @ Tipo Operacion : ${TIPO_OPERACION}" 
 echo "******************************************************"
-PARAMETROS="${TRANSACTION} ${FECHA_PROG} ${SERVC_ESTADO} ${SERVI_COD} ${TIPO_OPERACION}"
-
-"$JAVA_PATH"java -jar ${HOME_REQUEST}/$NAME_SHELL.jar ${PARAMETROS} >> $FILELOG
-
+echo ""$JAVA_PATH"java -jar ${HOME_REQUEST}/$NAME_SHELL.jar ${TRANSACTION}"
+"$JAVA_PATH"java -jar ${HOME_REQUEST}/$NAME_SHELL.jar ${TRANSACTION} >> $FILELOG
 DATE=`date +%Y-%m-%d@%H:%M:%S`
-
 ############### FIN - EJECUCION DE JAR ###############
 echo "${LOGDATE} INFO  @ [FIN de ejecucion de la Shell - $NAME_SHELL -][`date +%Y-%m-%d@%H:%M:%S`]"  
 echo "${DATE} INFO  @ Ruta del archivo log generado:${FILELOG}"  
