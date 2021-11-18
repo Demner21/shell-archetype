bbb850ce5fe182024b8332642c6c80494752667e
Demner <rlopezcr@everis.com>
2021-11-17 21:29:20 -0500

    basic container example
    container con java 11

---
+++ shell-archetype/runDocker.sh
@@ -0,0 +1,9 @@
+#if you do not have java 11 configured in your environment variables, 
+#you can change the path to the current terminal:
+
+#export JAVA_HOME='/c/Program Files/AdoptOpenJDK/jdk-11.0.9.101-hotspot'
+#export PATH=$JAVA_HOME/bin:$PATH
+
+./mvnw clean install -U
+docker build -t dmnr-java11 .
+docker run -d dmnr-java11
