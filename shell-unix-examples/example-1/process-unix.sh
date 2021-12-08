71df7f86615983c1baf0b71fdc1d9c615bd21801
Demner <rlopezcr@everis.com>
2021-12-08 17:53:10 -0500

    se han agregado otros ejemplos de invocaciones

--- shell-unix-examples/example-1/process-unix.sh
+++ shell-unix-examples/example-1/process-unix.sh
@@ -32,3 +32,50 @@ do
     #eliminando las ultimas 3 lineas
     #sed -i "$(( $(wc -l <$ruta/data/$FILE_PROCESSING)-3+1 )),$ d" $ruta/data/$FILE_PROCESSING 
 done
+
+
+echo " ejemplo de query en el mismo lugar de invocación"
+sqlplus -S $USRDB/$PASSDB@"$CONECTION_COMPLETE_DB" <<EOF > $ruta/data/$FILE_PROCESSING
+SET pagesize 0
+SET linesize 500
+SET feedback off
+SET termout off
+SET heading off
+SET head off
+SET verify off
+set echo off
+set serveroutput off
+
+    SELECT NAME_PARTNER,
+        ID_PARTNER
+    FROM TABLE_PARTNER;
+EXIT
+EOF
+
+echo " ejemplo de llamado a store procedure"
+RETVAL=`sqlplus -S $USRDB/$PASSDB@"$CONECTION_COMPLETE_DB"<<FIN 
+SET SERVEROUTPUT ON 
+SET SERVEROUTPUT ON FORMAT WORD_WRAPPED
+DECLARE
+P_cod_resultado VARCHAR2(10);
+P_msg_resultado VARCHAR2(50);
+BEGIN
+	  MY_OWNER.MY_PKG_EXAMPLE.MY_SP_EXAMPLE(P_cod_resultado,P_msg_resultado);
+	  dbms_output.put_line('respuesta de store procedure '|| P_cod_resultado ||' '|| P_msg_resultado);
+END;
+/
+EXIT
+FIN
+`
+k_cod_resultado=""
+k_msg_resultado=""
+
+k_cod_resultado=`echo $RETVAL | grep responseSP | awk '{print $2}'`
+k_msg_resultado=`echo $RETVAL | grep responseSP | awk '{print $3}'`
+echo "COD_RESPUESTA: $k_cod_resultado" >> $ruta/log/PROCESS.log
+echo "MSG_RESPUESTA: $k_msg_resultado" >> $ruta/log/PROCESS.log
+
+echo " ejemplo de sqlldr"
+export PATH=$PATH:~/databse-utils/instantclient_12_2
+
+sqlldr $CONECTION_COMPLETE_DB_SQL_LDR control=$ruta/ctl/load_data.ctl data=$ruta/data/test_data log=$ruta/log/PROCESS_SQL_LDR.log
